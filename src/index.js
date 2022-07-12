dcbd4bc1df9ad203a5b7ae131dd260269741b86d
hsoubcontrolpanel <hsoubcontrolpanel@gmail.com>
2022-07-12 10:41:26 +0300

    footer and index responsive done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 import '@fortawesome/fontawesome-free/js/all.min';
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
-console.log("hello")
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener('click', () => {
+        alert('أضيف المُنتج إلى عربة الشراء')
+    })
+})
+document.getElementById('copyright').innerHTML= "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear()
