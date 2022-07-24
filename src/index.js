cfb06dfcc4daf1ec90a43dd3b1a974dde97c8624
entesarEllBanna <entesar.2000banna@gmail.com>
2022-07-24 21:38:43 +0300

    update breadcrumb

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 import '@fortawesome/fontawesome-free/js/all.min';
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+
 document.querySelectorAll('.add-to-cart-btn').forEach(item => {
     item.addEventListener('click', () => {
         alert('أضيف المُنتج إلى عربة الشراء')
     })
 })
 document.getElementById('copyright').innerHTML= "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear()
+
+document.querySelectorAll(['.color-option input[type="radio"]']).forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove("active")
+        })
+        item.parentNode.parentNode.classList.add("active")
+    })
+})
+
+document.querySelectorAll(['.size-option input[type="radio"]']).forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove("active")
+        })
+        item.parentNode.parentNode.classList.add("active")
+    })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -94,6 +94,11 @@ module.exports = {
             template: "./src/index.html",
         }),
 
+        new HtmlWebpackPlugin({
+            filename: "product.html",
+            template: "./src/product.html",
+        }),
+
         new MiniCssExtractPlugin({
             filename: "css/style.css"
         }),
